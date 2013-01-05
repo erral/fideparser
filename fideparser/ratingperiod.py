bb6ec1c4f4855ffcf19ff3823d83bd4e9b3c6ca1
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-01-05 16:42:13 +0100

    skeleton of the parser

---
+++ fideparser/ratingperiod.py
@@ -0,0 +1,12 @@
+class RatingPeriod(object):
+    def __init__(self, country, period):
+        self.country = country
+        self.period = period
+
+    def save(self):
+        """ import the data from FIDE site """
+        pass
+
+    def export(self):
+        """ return the saved data in a structured way """
+        pass
