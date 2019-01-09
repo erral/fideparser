d8b0bcc252987f7a7833e592d39148b4d6062766
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-01-09 15:35:26 +0100

    fake tournament response with a fixed HTML

--- tests/test_ratingperiod.py
+++ tests/test_ratingperiod.py
@@ -3,6 +3,7 @@ from fideparser.ratingperiod import RatingPeriod
 
 import responses
 import unittest
+import re
 
 
 class TestArbiter(unittest.TestCase):
@@ -11,7 +12,9 @@ class TestArbiter(unittest.TestCase):
         ratingperiod_url = "https://ratings.fide.com/tournament_list.phtml?country=ESP&moder=ev_code&rating_period=2018-07-01"
         contents = get_test_file_contents("ratingperiod.html")
         responses.add(responses.GET, ratingperiod_url, body=contents)
-
+        tournament_url = re.compile("https:\/\/ratings\.fide\.com\/tournament_details\.phtml\?event=[\d]+")
+        tournament_contents = get_test_file_contents("tournament.html")
+        responses.add(responses.GET, tournament_url, body=tournament_contents)
         r = RatingPeriod("ESP", "2018-07-01", arbiters_data=False, report_data=False)
         r.save()
 
