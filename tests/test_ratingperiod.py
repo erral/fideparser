4fd853f39b0ca38068bfc366aae3d4cae862f117
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-01-10 08:54:17 +0100

    import/export tests

--- tests/test_ratingperiod.py
+++ tests/test_ratingperiod.py
@@ -1,9 +1,11 @@
+from .utils import get_path_to_store_export_files
 from .utils import get_test_file_contents
 from fideparser.ratingperiod import RatingPeriod
 
+import os
+import re
 import responses
 import unittest
-import re
 
 
 class TestArbiter(unittest.TestCase):
@@ -27,3 +29,53 @@ class TestArbiter(unittest.TestCase):
 
         self.assertIsInstance(r.tournaments, list)
         self.assertEqual(len(r.tournaments), 86)
+
+    @responses.activate
+    def test_ratingperiod_export_json(self):
+        r = RatingPeriod("ESP", "2018-07-01", arbiters_data=False, report_data=False)
+        r.save()
+        filename = "myjson.json"
+        export_path = get_path_to_store_export_files(filename)
+        r.export(export_path, "json")
+
+        dir_path = os.path.dirname(export_path)
+        self.assertIn(filename, os.listdir(dir_path))
+
+    @responses.activate
+    def test_ratingperiod_export_csv(self):
+        r = RatingPeriod("ESP", "2018-07-01", arbiters_data=False, report_data=False)
+        r.save()
+        filename = "myjson.csv"
+        export_path = get_path_to_store_export_files(filename)
+        r.export(export_path, "csv")
+
+        dir_path = os.path.dirname(export_path)
+        self.assertIn(filename, os.listdir(dir_path))
+
+    @responses.activate
+    def test_ratingperiod_export_binary(self):
+        r = RatingPeriod("ESP", "2018-07-01", arbiters_data=False, report_data=False)
+        r.save()
+        filename = "myjson.binary"
+        export_path = get_path_to_store_export_files(filename)
+        r.export(export_path, "binary")
+
+        dir_path = os.path.dirname(export_path)
+        self.assertIn(filename, os.listdir(dir_path))
+
+    @responses.activate
+    def test_ratingperiod_import_binary(self):
+        """Check that exported binary can be imported """
+        r = RatingPeriod("ESP", "2018-07-01", arbiters_data=False, report_data=False)
+        r.save()
+        filename = "myjson.binary"
+        export_path = get_path_to_store_export_files(filename)
+        r.export_binary(export_path)
+
+        dir_path = os.path.dirname(export_path)
+        self.assertIn(filename, os.listdir(dir_path))
+
+        r_imported = RatingPeriod("ESP", "2018-07-01")
+        r_imported.load_from_file(export_path)
+
+        self.assertEquals(r, r_imported)
--- tests/utils.py
+++ tests/utils.py
@@ -3,5 +3,11 @@ import os
 
 def get_test_file_contents(filename):
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    arbiter_url_file = open(os.path.join(dir_path, "fide_responses", filename))
-    return arbiter_url_file.read()
+    fp = open(os.path.join(dir_path, "fide_responses", filename))
+    return fp.read()
+
+
+def get_path_to_store_export_files(filename):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, "fide_responses", filename)
+    return file_path
